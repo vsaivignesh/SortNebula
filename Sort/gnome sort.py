3f412e9a284c6d96f0a2166d7b74b40bb20e144b
V Sai Vignesh <v.saivignesh2000@gmail.com>
2019-05-22 01:19:28 +0530

    Changed dictionary to list

--- Sort/gnome sort.py
+++ Sort/gnome sort.py
@@ -1,14 +1,14 @@
-thisdict={}
+arr=[]
 n=int(input(print("Enter number of keys")))
 for i in range(n):
     print("Enter key")
     a=int(input())
     print("Enter value")
-    thisdict[a]=int(input())
-arr=list(thisdict.keys())
+    b=int(input())
+    arr.append([a,b])
 j=0
 while(j<len(arr)-1):
-    if arr[j]>arr[j+1]:
+    if arr[j][0]>arr[j+1][0]:
         tmep=arr[j+1]
         arr[j+1]=arr[j]
         arr[j]=tmep
@@ -16,4 +16,4 @@ while(j<len(arr)-1):
             j=j-2
     j=j+1
 for i in range(len(arr)):
-    print(str(arr[i])+": "+str(thisdict.get(arr[i])))
+    print(str(arr[i][0])+": "+str(arr[i][1]))
